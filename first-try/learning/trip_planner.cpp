c64d1ab311931bc28aa0ec57195a60167c4822c5
Zak Gilliam <zakgilliam@gmail.com>
2025-11-25 14:12:23 -0600

    first commit

--- first-try/learning/trip_planner.cpp
+++ first-try/learning/trip_planner.cpp
@@ -2,11 +2,27 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <chrono>
+#include <thread>
 using namespace std;
+using namespace std::this_thread; // sleep_for, sleep_until
+using namespace std::chrono; // nanoseconds, system_clock, seconds
 
-int main() {
+int main() 
+{
+  string number_input;
+  int number = 0;
 
+  cout << "Enter the number to start the countdown: ";
+  getline(cin, number_input);
+  stringstream(number_input) >> number;
 
+  while (number > 0)
+    {
+      cout << number << endl;
+      number--;
+      sleep_for(seconds(1));
+    }
 
+  cout << "BOOM!!" << endl;
 }
-
